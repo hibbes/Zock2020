7a1e0902117a9d29d1a4027b1945ef79d3a350af
marek <marek@DESKTOP-RI7VA5D>
2021-01-11 14:01:41 +0100

    SimpleOval initial

---
+++ Zocken2020/src/SimpleOval.java
@@ -0,0 +1,23 @@
+public class SimpleOval extends GeometricObject {
+
+	public SimpleOval(double w, double h, Vertex pos) {
+		this.width=w;
+		this.height=h;
+		this.pos=pos;
+	}
+	
+	public SimpleOval(double w, double h, double x, double y) {
+		super(w,h,new Vertex(x,y));
+	}
+	
+	@Override public double area() {return Math.PI*width*height/4;}
+
+	public @Override String toString() {
+		return "SimpleOval("+super.toString()+")";
+	}
+	
+	public @Override boolean equals(Object that) {
+		return (that instanceof SimpleOval) && super.equals(that);
+	}
+	
+}
--- Zocken2020/src/TestGeometricObject.java
+++ Zocken2020/src/TestGeometricObject.java
@@ -2,10 +2,11 @@
 public class TestGeometricObject {
 
 	public static void main(String[] args) {
-		GeometricObject o1 = new GeometricObject(17,4, new Vertex(42,23));
-		GeometricObject o2= new GeometricObject(17,4);
-		GeometricObject o3= new GeometricObject(17);
-		GeometricObject o4= new GeometricObject();
+		SimpleOval o1 = new SimpleOval(17,4, new Vertex(42,23));
+		SimpleOval o2= new SimpleOval(17,4, 0,0);
+		SimpleOval o3= new SimpleOval(17,42, 67, 78);
+		SimpleOval o4= new SimpleOval(3,3,3,3);
+		
 
 		System.out.println(o1.equals(o2));
 		o2.moveTo(42,23);
