e7259a212ca2f91f817cc9fd6f47a70b99654684
marek <marek@MathesPC>
2021-02-22 11:59:33 +0100

    MovablePanel

---
+++ Zocken2020/src/MovablePanel.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class MovablePanel extends SizedPanel{
+	List<GameObject> gos= new ArrayList<GameObject>();
+	
+	public void paintComponent(java.awt.Graphics g) {
+		super.paintComponent(g);
+		for (GameObject go:gos) go.paintTo(g);
+	}
+	public void move() {for (GameObject go:gos) go.move();}
+	
+	public void doOnTick() {
+		move();
+		repaint();
+	}
+
+}
